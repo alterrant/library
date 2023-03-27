5ccfdbc328892a0a9440cbdf356c96014d5e3a15
alterrant <pandor246@mail.ru>
2023-03-27 05:08:44 +0300

    fix(*): handle getGenres error

--- src/entities/nav-lists/model/saga.tsx
+++ src/entities/nav-lists/model/saga.tsx
@@ -1,6 +1,7 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 
 import { axiosInstance, BOOKS_API } from 'shared/api';
+import { ErrorMessages } from 'shared/lib';
 import { getGenres, setGenres } from '.';
 import { GenreType } from '../lib';
 
@@ -10,13 +11,9 @@ function* worker() {
 
     yield put(setGenres(data as GenreType[]));
   } catch {
-    // вынужденно отключаем обработку ошибок из-за 5157,5158 и 5608,5609 строк sprint7:
-    // на 5158 обязательно сделать запрос categories при переходе на /profile, а потом
-    // категорически нельзя делать запрос categories при переходе на /profile. Если сделаем -
-    // упадём с ошибкой. Другой костыль был бы: отключить отображение ошибки запроса categories
-    // только в профиле, но суть костыля такая была бы же: ошибка есть, но я вам её не покажу
-
-    /* yield put(setGenresError(ErrorMessages.RELOAD_PAGE)); */
+    // из-за 5157,5158 и 5608,5609 строк sprint7 обрабатываем ошибку только консолькой, без
+    // прокидывания в стор
+    console.log(ErrorMessages.RELOAD_PAGE);
   }
 }
 
